15087ed6dcaee07b11d422f536b2a869dcb545ca
Vehikl Team <go@vehikl.com>
2019-06-24 15:33:55 -0400

    Position avatar within cup handle

--- resources/js/components/TweetCard.vue
+++ resources/js/components/TweetCard.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="tweet-card">
+        <div class="border-look-alike"/>
         <img class="user-avatar" :src="`https://robohash.org/${ message }?set=set4`">
-        <div class="card-body" v-text="message"></div>
+        <div class="message-container" v-text="message"></div>
     </div>
 </template>
 
@@ -17,16 +18,28 @@
 <style scope lang="scss">
     @import '../../sass/variables';
 
+    $_cupHandleDistance: $size-lg;
+    $_cupHandleBorderThickness: $size-md/2;
+    $_avatarSize: $size-7xl;
+
     .tweet-card {
+        display: flex;
+        align-items: center;
+        position: relative;
+    }
+
+    .message-container {
         background-color: $tweetGrey;
         border-radius: $size-lg $size-lg $size-5xl $size-5xl;
         box-shadow: 0 4px 10px 0 hsla(0, 0, 0, 0.1);
         min-height: $size-8xl;
         width: 100%;
+        z-index: 2;
+        padding: $size-2xl $size-5xl;
+    }
 
-        &:hover {
-            background-color: var(--my-color);
-        }
+    .tweet-card {
+        display: flex;
     }
 
     .card-body {
@@ -35,7 +48,24 @@
     }
 
     img {
-        height: $size-7xl;
+        background-color: $tweetGrey;
+        height: $_avatarSize;
         clip-path: circle(50%);
+        position: absolute;
+        top: $_cupHandleDistance + $_cupHandleBorderThickness;
+        left: -$_avatarSize/2;
+        z-index: 3;
+    }
+
+    .border-look-alike {
+        background-color: $tweetGrey;
+        box-shadow: 0 4px 10px 0 hsla(0, 0, 0, 0.1);
+        height: $_avatarSize + $_cupHandleBorderThickness * 2;
+        width: $_avatarSize + $_cupHandleBorderThickness * 2;
+        border-radius: 50%;
+        position: absolute;
+        top: $_cupHandleDistance;
+        left: -($_avatarSize + $size-md)/2;
+        z-index: 1;
     }
 </style>
