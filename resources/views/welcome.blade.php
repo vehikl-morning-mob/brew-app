92ca7db58892b7c6fb888d0a3b0f915b7138386d
Vehikl Team <go@vehikl.com>
2019-07-08 14:49:37 -0400

    Use app layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,6 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{csrf_token()}}">
-    <script src="{{ mix('js/app.js') }}" defer></script>
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:400,900&display=swap" rel="stylesheet">
-    <title>Laravel</title>
-
-</head>
-<body>
-<div id="app">
+@extends('layouts.app')
+@section('content')
     <twitter-app :min-tweet-length="{{ $minTweetLength }}"
                  :max-tweet-length="{{ $maxTweetLength }}"
     ></twitter-app>
-</div>
-</body>
-</html>
+@endsection
