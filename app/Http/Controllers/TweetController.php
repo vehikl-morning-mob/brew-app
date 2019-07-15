8e94772a9da19a50f49ac34fc260db42db1c5ed9
Vehikl Team <go@vehikl.com>
2019-07-15 15:08:57 -0400

    Provide user avatar on tweets list

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -19,7 +19,7 @@ class TweetController extends Controller
         $allTweets = Tweet::orderBy('id', 'DESC')->get()->map(function ($tweetItem) {
             return [
                 'userName' => $tweetItem->user->name,
-                'avatar' => '',
+                'avatar' => $tweetItem->user->avatar,
                 'message' => $tweetItem->message,
             ];
         });
@@ -48,7 +48,7 @@ class TweetController extends Controller
         $newTweet = $request->user()->tweets()->create($request->validated());
         return response([
             'message' => $newTweet->message,
-            'avatar' => '',
+            'avatar' => $newTweet->user->avatar,
             'userName' => $newTweet->user->name,
         ], Response::HTTP_CREATED);
     }
