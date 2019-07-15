20a804a3fea5f0ffb219dbf0637c1c20eccedf18
Vehikl Team <go@vehikl.com>
2019-07-15 15:15:14 -0400

    Refactor test

--- resources/js/components/__tests__/RegistrationForm.spec.ts
+++ resources/js/components/__tests__/RegistrationForm.spec.ts
@@ -5,6 +5,13 @@ import axios from 'axios';
 import MockAdapter from "axios-mock-adapter";
 import ImageUploader from "../ImageUploader.vue";
 
+const user = {
+    name: 'foobar',
+    email: 'foo@bar.com',
+    password: 'foofoobarb4r',
+    avatar: 'whateverbar'
+};
+
 describe('RegistrationForm', () => {
     let wrapper: Wrapper<RegistrationForm>;
     let mockServer: MockAdapter;
@@ -26,38 +33,37 @@ describe('RegistrationForm', () => {
     });
 
     it('Displays error if backend validation fails', async () => {
-
         const emailErrorMessage = 'Email already taken!';
         mockServer.onPost('/register').reply(422, {
             errors: {
                 email: [emailErrorMessage]
             }
         });
-        wrapper.find('#email').setValue('t@s.alad');
-        wrapper.find('#name').setValue('tacos');
-        wrapper.find('#password').setValue('localTaco');
-        wrapper.find('#password-confirm').setValue('localTaco');
+
+        fillFormFields();
 
         wrapper.find('#submit').trigger('click');
-
         await flushPromises();
 
         expect(wrapper.text()).toContain(emailErrorMessage);
     });
 
     it('Sets uploaded image as avatar', async () => {
-        const userAvatar: string = 'hello123';
-        wrapper.find(ImageUploader).vm.$emit('image-uploaded', userAvatar);
-        wrapper.find('#email').setValue('t@s.alad');
-        wrapper.find('#name').setValue('tacos');
-        wrapper.find('#password').setValue('localTaco');
-        wrapper.find('#password-confirm').setValue('localTaco');
+        fillFormFields();
 
         wrapper.find('#submit').trigger('click');
         await flushPromises();
         const registrationPayload = JSON.parse(mockServer.history.post[0].data);
 
-        expect(registrationPayload.avatar).toBe(userAvatar)
+        expect(registrationPayload.avatar).toBe(user.avatar)
     });
+
+    function fillFormFields() {
+        wrapper.find('#email').setValue(user.email);
+        wrapper.find('#name').setValue(user.name);
+        wrapper.find('#password').setValue(user.password);
+        wrapper.find('#password-confirm').setValue(user.password);
+        wrapper.find(ImageUploader).vm.$emit('image-uploaded', user.avatar);
+    }
 
 });
